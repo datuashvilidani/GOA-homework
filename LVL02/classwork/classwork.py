af4003c4dfb15e97a102d25398afe9bbb9b4d7b7
datuashvilidani <datuashvilidani@gmail.com>
2025-02-16 13:41:57 +0100

    commit

---
+++ LVL02/classwork/classwork.py
@@ -0,0 +1,15 @@
+#data type
+
+# string 
+# integer
+# float 
+# boolean
+
+'hello dani'
+11
+3.4
+True
+False
+ 
+"hello" * 2
+("hello" + (10))
---
+++ LVL02/homework/homework.py
@@ -0,0 +1,15 @@
+a = "string"
+b = 10
+greeting = "gamarjoba"
+e = "elementi"
+print(e*10)
+
+c = 500
+d = 500
+print(c+d)
+
+h = "hello"
+w = "world"
+print(h + w)
+ 
+10>5
