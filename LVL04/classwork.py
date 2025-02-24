24818e483ba5e4972a35e7958820d214acfc7973
datuashvilidani <datuashvilidani@gmail.com>
2025-02-24 19:55:19 +0100

    commit

---
+++ LVL04/classwork.py
@@ -0,0 +1,64 @@
+#x=int(input("ramdeni wlis xar"))
+
+#if x>=18:
+ #   print("you can enter")
+
+#elif x>16:
+#    print("you are allowed to drive")
+     
+     
+#else:
+#    print("you are not allowed on this site")
+
+
+
+#num1= int(input("enter first num"))
+#if num1%2==0:
+  #print("luwia")
+#else:
+   #print("kentia")
+
+
+#num = int(input("first num"))
+#if num==0:
+#    print("es ricxvi nulia")
+#elif num==1:
+  #  print("es ricxvi pozitiuria")
+
+#elif num==-1:
+ #   print("es cifri negatiuria")
+
+
+#num1=int(input("your first number"))
+#num2=int(input("your second number"))
+
+#if num1>num2:
+ #   print(num1)
+#else:
+ #   print(num2)
+
+
+# x=int(input("your year"))
+# if x>=2000:
+#     print("axali saukune")
+# elif x<2000:
+#     print("dzveli saukune")
+ 
+x=int(input("your number"))
+
+if x/5==0:
+    print(x)
+else:
+    print("0")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LVL04/homework.py
@@ -0,0 +1,40 @@
+# age=int(input("enter your age"))
+# if age>=18:
+#     print("უნდა იაროს სამსახურში")
+# elif age>=6:
+#         print("უნდა იაროს სკოლაში")
+ 
+# elif age>=2:
+#       print("უნდა იაროს ბაღში")
+
+
+
+
+# academy=input("რომელ აკადემიაში დადიხარ?")
+
+# if academy=="Goa":
+#     print("U r a real chad")
+# elif academy=="გოა":
+#     print("U r a real chad")
+# elif academy=="goa":
+#     print("U r a real chad")
+
+# else:
+#     print("join goa, become a chad")
+
+
+
+score=int(input("How much did you score?"))
+
+if score>=90:
+    print("A")
+elif score>=75:
+    print("B")
+elif score>=60:
+    print("C")
+elif score>=50:
+    print("D")
+elif score>=40:
+    print("E")
+elif score>=30:
+    print("F,Good luck💚")
