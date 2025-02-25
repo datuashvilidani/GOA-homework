b1710cad15f300126c60fcd762bd5ddf6b85abb1
datuashvilidani <datuashvilidani@gmail.com>
2025-02-25 19:02:36 +0100

    commit

---
+++ LVL05/classwork/classwork.py
@@ -0,0 +1,29 @@
+# for i in range(10):
+#     print(i, "hello world")
+    
+
+# for i in range(2,25):
+#     if i % 2 !=0:
+#         print(i)
+
+
+# name=input("your name")
+
+# for i in name:
+#     print(i)
+
+
+# seats = 10 
+# while seats > 0:
+#     print('seat sold')
+#     seats -= 1
+# if seats==0:
+#     print("Sold out")
+
+
+correct_password = "samirviani"
+paroli=input("shemoiyvane paroli")
+while paroli != correct_password:
+    print("scade axlidan")
+    paroli=input("shemoiyvane paroli")
+print("paroli sworia")
