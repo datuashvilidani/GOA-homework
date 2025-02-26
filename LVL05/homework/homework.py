2639282950b611b2f06d040cc0031c99f936629c
datuashvilidani <datuashvilidani@gmail.com>
2025-02-26 13:36:30 +0100

    commit

---
+++ LVL05/homework/homework.py
@@ -0,0 +1,18 @@
+# for i  in range (1,10):
+#    print(i)
+
+
+# for i in range (5,25):
+#    print(i)
+
+
+# for i in range (10,100,5):
+#      print(i)
+
+name=input("Shemoitane sityva")
+
+res= ""
+for i in name:
+    if i =="A":
+        res+= i
+print(res)
